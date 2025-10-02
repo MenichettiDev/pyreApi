c18fd18b80657cea1e55207f91c6e2d6cccbf2df
MenichettiDev <menichetti.dev@gmail.com>
2025-10-01 23:10:22 -0300

    Feat - Obra - Eliminacion de metodos repetidos en controller

--- Controllers/ObraController.cs
+++ Controllers/ObraController.cs
@@ -18,50 +18,5 @@ namespace pyreApi.Controllers
             return entity?.IdObra ?? 0;
         }
 
-        // [HttpPost("create")]
-        // public async Task<IActionResult> CreateFromDto([FromBody] CreateObraDto dto)
-        // {
-        //     if (!ModelState.IsValid)
-        //         return BadRequest(ModelState);
-
-        //     var obra = new Obra
-        //     {
-        //         Codigo = dto.Codigo,
-        //         NombreObra = dto.NombreObra,
-        //         Ubicacion = dto.Ubicacion,
-        //         FechaInicio = dto.FechaInicio,
-        //         FechaFin = dto.FechaFin,
-        //     };
-
-        //     var response = await _service.AddAsync(obra);
-        //     if (response.Success)
-        //         return CreatedAtAction(nameof(GetById), new { id = response.Data?.IdObra }, response);
-        //     return BadRequest(response);
-        // }
-
-        [HttpPut("update/{id}")]
-        public async Task<IActionResult> UpdateFromDto(int id, [FromBody] UpdateObraDto dto)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            if (id != dto.IdObra)
-                return BadRequest("ID mismatch");
-
-            var obra = new Obra
-            {
-                IdObra = dto.IdObra,
-                Codigo = dto.Codigo,
-                NombreObra = dto.NombreObra,
-                Ubicacion = dto.Ubicacion,
-                FechaInicio = dto.FechaInicio,
-                FechaFin = dto.FechaFinEstimada,
-            };
-
-            var response = await _service.UpdateAsync(obra);
-            if (response.Success)
-                return Ok(response);
-            return BadRequest(response);
-        }
     }
 }
