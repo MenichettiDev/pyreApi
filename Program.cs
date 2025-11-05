d8a925a16c23452e5a13d20f1489fe6521c74b37
MenichettiDev <menichetti.dev@gmail.com>
2025-11-05 00:20:26 -0300

    Fix - Puerto de inicio 1000

--- Program.cs
+++ Program.cs
@@ -249,7 +249,7 @@ app.MapControllers(); // esto es para habilitar los enroutadores de los controla
 //Original
 // app.Run();
 //Cambio
-app.Run("http://localhost:5000");
+app.Run("http://localhost:1000");
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
