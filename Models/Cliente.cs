dfffaeb69c900e1a28ecbf754e9b8a4152f48941
MenichettiDev <menichetti.dev@gmail.com>
2025-11-17 23:16:20 -0300

    Feat - Cliente - Creacion de la entidad - Controller - Service - Dtos -
    Fix  -Obras - Se agrego el id cliente en las relaciones y en los Dtos de obras

---
+++ Models/Cliente.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace pyreApi.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int IdCliente { get; set; }
+        [MaxLength(11)]
+        public string? Cuit { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Nombre { get; set; } = string.Empty;
+        [MaxLength(50)]
+        public string? Telefono { get; set; }
+        [MaxLength(150)]
+        public string? Email { get; set; }
+        [MaxLength(255)]
+        public string? Direccion { get; set; }
+        public bool Activo { get; set; }
+        public DateTime FechaRegistro { get; set; }
+    }
+}
